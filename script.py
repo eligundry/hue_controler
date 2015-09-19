a950097b617549475c3391e3aa76373fc66d0eee
Eli Gundry <eligundry@gmail.com>
2015-09-19 12:05:45 -0400

    Inital commit loading config file

---
+++ script.py
@@ -0,0 +1,13 @@
+import ConfigParser
+
+import requests
+
+# Load config file
+config = ConfigParser.ConfigParser()
+config.readfp(open('config.cfg'))
+
+def create_user():
+    pass
+
+def authenticate():
+    pass
