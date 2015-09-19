9b7244aef9c5d9a987ec7aec1259df02393de79d
Eli Gundry <eligundry@gmail.com>
2015-09-19 12:53:56 -0400

    Adds authentication and user creation functions

--- script.py
+++ script.py
@@ -6,8 +6,34 @@ import requests
 config = ConfigParser.ConfigParser()
 config.readfp(open('config.cfg'))
 
+api_endpoint = 'http://{}/api'.format(config.get('hue', 'ip_address'))
+
 def create_user():
-    pass
+    """
+    Attempts to create a new authenticated user for the Hue. It will throw an
+    exception if the link button has been pressed yes.
+    """
+    payload = { 'deveicetype': config.get('hue', 'devicetype') }
+    r = requests.post(api_endpoint, json=payload)
+    return r
+
+    if 'error' in r.json()[0]:
+        raise Exception(r.json()[0]['error']['description'])
+
+    return True
 
 def authenticate():
-    pass
+    """
+    Authenticates the given devicetype provided in the config file. If the
+    devicetype is not a valid user, it will attempt to create the user using
+    the Hue API.
+    """
+    r = requests.get('{}/newdeveloper'.format(api_endpoint))
+
+    if 'error' in r.json()[0]:
+        return create_user()
+
+    return True
+
+if __name__ == '__main__':
+    authenticate()
